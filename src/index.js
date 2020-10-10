724ed07e3aa3774af52ba13689a20e3dfb4c457f
Steve Brownlee <chortlehoort@gmail.com>
2020-10-10 11:06:22 -0500

    Cleaned up README

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom"
 import { Rare } from "./components/Rare.js"
 import "./index.css"
 
-localStorage.setItem("rare_user_id", 1)
-
 ReactDOM.render(
     <React.StrictMode>
         <Router>
